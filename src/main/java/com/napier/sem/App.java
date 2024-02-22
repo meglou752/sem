15aaadb71cb7362ba18989f0498b568839272f00
meglou782 <meganmcleod@Megans-MacBook-Pro-4.local>
2024-02-22 16:11:41 +0000

    Adding all-salaries feature, now printing.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -16,7 +16,7 @@ public class App
         // Extract employee salary information
         ArrayList<Employee> employees = a.getAllSalaries();
 
-        // Test the size of the returned data - should be 240124
+        // All salaries in employee (all departments)
         a.printSalaries(employees);
 
         // Disconnect from database
