b86885db09ad7ad3d44ace2433b8b8b5a94be7f1
meglou782 <meganmcleod@Megans-MacBook-Pro-4.local>
2024-03-07 16:15:20 +0000

    Adding unit tests.

---
+++ src/test/java/com/napier/sem/MyTest.java
@@ -0,0 +1,99 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+    }
+
+    @Test
+    void printSalariesTestNull()
+    {
+        app.printSalaries(null);
+    }
+
+    @Test
+    void printSalariesTestEmpty()
+    {
+        ArrayList<Employee> employees = new ArrayList<Employee>();
+        app.printSalaries(employees);
+    }
+
+    @Test
+    void printSalariesTestContainsNull()
+    {
+        ArrayList<Employee> employees = new ArrayList<Employee>();
+        employees.add(null);
+        app.printSalaries(employees);
+    }
+
+    @Test
+    void printSalaries()
+    {
+        ArrayList<Employee> employees = new ArrayList<Employee>();
+        Employee emp = new Employee();
+        emp.emp_no = 1;
+        emp.first_name = "Kevin";
+        emp.last_name = "Chalmers";
+        emp.title = "Engineer";
+        emp.salary = 55000;
+        employees.add(emp);
+        app.printSalaries(employees);
+    }
+
+    @Test
+    void displayEmpTestNull()
+    {
+        app.displayEmployee(null);
+    }
+
+    @Test
+    void displayEmpTestEmpty()
+    {
+        ArrayList<Employee> employees = new ArrayList<Employee>();
+        for(Employee emp : employees)
+        {
+            app.displayEmployee(emp);
+        }
+    }
+
+    @Test
+    void displayEmpTestContainsNull()
+    {
+        ArrayList<Employee> employees = new ArrayList<Employee>();
+        employees.add(null);
+        for(Employee emp : employees)
+        {
+            app.displayEmployee(emp);
+        }
+    }
+
+    @Test
+    void displayEmployee()
+    {
+        ArrayList<Employee> employees = new ArrayList<Employee>();
+        Employee emp = new Employee();
+        emp.emp_no = 1;
+        emp.first_name = "Kevin";
+        emp.last_name = "Chalmers";
+        emp.title = "Engineer";
+        emp.salary = 55000;
+        employees.add(emp);
+        for(Employee employee : employees)
+        {
+            app.displayEmployee(employee);
+        }
+    }
+}
